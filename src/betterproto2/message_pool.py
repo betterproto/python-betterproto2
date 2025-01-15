9d8fc0f44a7625777390cd9d2d692445b4b7a0ac
Adrien Vannson <adrien.vannson@protonmail.com>
2025-01-15 19:06:19 +0100

    Define global message pool

--- src/betterproto2/message_pool.py
+++ src/betterproto2/message_pool.py
@@ -25,8 +25,12 @@ class MessagePool:
     def register_message(self, package_name: str, message_name: str, message_type: "type[Message]") -> None:
         url = get_type_url(package_name, message_name)
 
-        if url in self.url_to_type or message_type or self.type_to_url:
+        if url in self.url_to_type or message_type in self.type_to_url:
             raise RuntimeError(f"the message {package_name}.{message_name} is already registered in the message pool")
 
         self.url_to_type[url] = message_type
         self.type_to_url[message_type] = url
+
+
+default_message_pool = MessagePool()
+"""Global message pool that contains, by default, all the imported betterproto messages."""
