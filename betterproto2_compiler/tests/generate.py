4b23f95baa2ac6d05b99a0283adc66050cd5f7bf
Adrien Vannson <adrien.vannson@protonmail.com>
2025-07-01 12:54:39 +0200

    Improve test generation

--- betterproto2_compiler/tests/generate.py
+++ betterproto2_compiler/tests/generate.py
@@ -2,16 +2,15 @@
 import asyncio
 import os
 import shutil
-import sys
-from pathlib import Path
 
 from tests.util import (
-    get_directories,
-    inputs_path,
-    output_path_betterproto,
-    output_path_betterproto_descriptor,
-    output_path_betterproto_pydantic,
-    output_path_reference,
+    # TODO delete definitions?
+    # get_directories,
+    # inputs_path,
+    # output_path_betterproto,
+    # output_path_betterproto_descriptor,
+    # output_path_betterproto_pydantic,
+    # output_path_reference,
     protoc,
 )
 
@@ -20,148 +19,91 @@ from tests.util import (
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
 
-def clear_directory(dir_path: Path):
-    for file_or_directory in dir_path.glob("*"):
-        if file_or_directory.is_dir():
-            shutil.rmtree(file_or_directory)
-        else:
-            file_or_directory.unlink()
+# async def generate(verbose: bool):
+#     failed_test_cases = []
+#     # Wait for all subprocs and match any failures to names to report
+#     for test_case_name, result in zip(sorted(test_case_names), await asyncio.gather(*generation_tasks)):
+#         if result != 0:
+#             failed_test_cases.append(test_case_name)
+
+#     if len(failed_test_cases) > 0:
+#         sys.stderr.write("\n\033[31;1;4mFailed to generate the following test cases:\033[0m\n")
+#         for failed_test_case in failed_test_cases:
+#             sys.stderr.write(f"- {failed_test_case}\n")
+
+#         sys.exit(1)
 
 
-async def generate(verbose: bool):
-    test_case_names = set(get_directories(inputs_path)) - {"__pycache__"}
+async def generate_test(
+    name,
+    semaphore: asyncio.Semaphore,
+    *,
+    reference: bool = False,
+    pydantic: bool = False,
+    descriptors: bool = False,
+):
+    await semaphore.acquire()
 
-    generation_tasks = []
-    for test_case_name in sorted(test_case_names):
-        test_case_input_path = inputs_path.joinpath(test_case_name).resolve()
-        generation_tasks.append(generate_test_case_output(test_case_input_path, test_case_name, verbose))
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    failed_test_cases = []
-    # Wait for all subprocs and match any failures to names to report
-    for test_case_name, result in zip(sorted(test_case_names), await asyncio.gather(*generation_tasks)):
-        if result != 0:
-            failed_test_cases.append(test_case_name)
+    options = []
+    if reference:
+        options.append("reference")
+    if pydantic:
+        options.append("pydantic")
+    if descriptors:
+        options.append("descriptors")
 
-    if len(failed_test_cases) > 0:
-        sys.stderr.write("\n\033[31;1;4mFailed to generate the following test cases:\033[0m\n")
-        for failed_test_case in failed_test_cases:
-            sys.stderr.write(f"- {failed_test_case}\n")
+    input_dir = dir_path + "/inputs/" + name
+    output_dir = dir_path + "/outputs/" + name + ("_" + "_".join(options) if options else "")
 
-        sys.exit(1)
+    os.mkdir(output_dir)
 
-
-async def generate_test_case_output(test_case_input_path: Path, test_case_name: str, verbose: bool) -> int:
-    """
-    Returns the max of the subprocess return values
-    """
-
-    test_case_output_path_reference = output_path_reference.joinpath(test_case_name)
-    test_case_output_path_betterproto = output_path_betterproto
-    test_case_output_path_betterproto_pyd = output_path_betterproto_pydantic
-    test_case_output_path_betterproto_desc = output_path_betterproto_descriptor
-
-    os.makedirs(test_case_output_path_reference, exist_ok=True)
-    os.makedirs(test_case_output_path_betterproto, exist_ok=True)
-    os.makedirs(test_case_output_path_betterproto_pyd, exist_ok=True)
-    os.makedirs(test_case_output_path_betterproto_desc, exist_ok=True)
-
-    clear_directory(test_case_output_path_reference)
-    clear_directory(test_case_output_path_betterproto)
-    clear_directory(test_case_output_path_betterproto_pyd)
-    clear_directory(test_case_output_path_betterproto_desc)
-
-    (
-        (ref_out, ref_err, ref_code),
-        (plg_out, plg_err, plg_code),
-        (plg_out_pyd, plg_err_pyd, plg_code_pyd),
-        (plg_out_desc, plg_err_desc, plg_code_desc),
-    ) = await asyncio.gather(
-        protoc(test_case_input_path, test_case_output_path_reference, True),
-        protoc(test_case_input_path, test_case_output_path_betterproto, False),
-        protoc(test_case_input_path, test_case_output_path_betterproto_pyd, False, True),
-        protoc(test_case_input_path, test_case_output_path_betterproto_desc, False, False, True),
+    stdout, stderr, returncode = await protoc(
+        input_dir,
+        output_dir,
+        reference=reference,
+        pydantic_dataclasses=pydantic,
+        google_protobuf_descriptors=descriptors,
     )
 
-    if ref_code == 0:
-        print(f"\033[31;1;4mGenerated reference output for {test_case_name!r}\033[0m")
+    if options:
+        options_str = ", ".join(options)
+        options_str = f" ({options_str})"
     else:
-        print(f"\033[31;1;4mFailed to generate reference output for {test_case_name!r}\033[0m")
-        print(ref_err.decode())
+        options_str = ""
 
-    if verbose:
-        if ref_out:
-            print("Reference stdout:")
-            sys.stdout.buffer.write(ref_out)
-            sys.stdout.buffer.flush()
-
-        if ref_err:
-            print("Reference stderr:")
-            sys.stderr.buffer.write(ref_err)
-            sys.stderr.buffer.flush()
-
-    if plg_code == 0:
-        print(f"\033[31;1;4mGenerated plugin output for {test_case_name!r}\033[0m")
+    if returncode == 0:
+        print(f"\033[31;1;4mGenerated output for {name!r}{options_str}\033[0m")
     else:
-        print(f"\033[31;1;4mFailed to generate plugin output for {test_case_name!r}\033[0m")
-        print(plg_err.decode())
+        print(f"\033[31;1;4mFailed to generate reference output for {name!r}{options_str}\033[0m")
+        print(stderr.decode())
 
-    if verbose:
-        if plg_out:
-            print("Plugin stdout:")
-            sys.stdout.buffer.write(plg_out)
-            sys.stdout.buffer.flush()
+    semaphore.release()
 
-        if plg_err:
-            print("Plugin stderr:")
-            sys.stderr.buffer.write(plg_err)
-            sys.stderr.buffer.flush()
 
-    if plg_code_pyd == 0:
-        print(f"\033[31;1;4mGenerated plugin (pydantic compatible) output for {test_case_name!r}\033[0m")
-    else:
-        print(f"\033[31;1;4mFailed to generate plugin (pydantic compatible) output for {test_case_name!r}\033[0m")
-        print(plg_err_pyd.decode())
+async def main_async():
+    # Don't compile too many tests in parallel
+    semaphore = asyncio.Semaphore(4)
 
-    if verbose:
-        if plg_out_pyd:
-            print("Plugin stdout:")
-            sys.stdout.buffer.write(plg_out_pyd)
-            sys.stdout.buffer.flush()
-
-        if plg_err_pyd:
-            print("Plugin stderr:")
-            sys.stderr.buffer.write(plg_err_pyd)
-            sys.stderr.buffer.flush()
-
-    if plg_code_desc == 0:
-        print(f"\033[31;1;4mGenerated plugin (google protobuf descriptor) output for {test_case_name!r}\033[0m")
-    else:
-        print(
-            f"\033[31;1;4mFailed to generate plugin (google protobuf descriptor) output for {test_case_name!r}\033[0m"
-        )
-        print(plg_err_desc.decode())
-
-    if verbose:
-        if plg_out_desc:
-            print("Plugin stdout:")
-            sys.stdout.buffer.write(plg_out_desc)
-            sys.stdout.buffer.flush()
-
-        if plg_err_desc:
-            print("Plugin stderr:")
-            sys.stderr.buffer.write(plg_err_desc)
-            sys.stderr.buffer.flush()
-
-    return max(ref_code, plg_code, plg_code_pyd, plg_code_desc)
+    tasks = [
+        generate_test("any", semaphore, reference=True),
+        generate_test("any", semaphore),
+        generate_test("any", semaphore, pydantic=True),
+        generate_test("any", semaphore, descriptors=True),
+        generate_test("any", semaphore, pydantic=True, descriptors=True),
+    ]
+    await asyncio.gather(*tasks)
 
 
 def main():
-    if sys.argv[1:2] == ["-v"]:
-        verbose = True
-    else:
-        verbose = False
+    # Clean the output directory
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    asyncio.run(generate(verbose))
+    shutil.rmtree(dir_path + "/outputs", ignore_errors=True)
+    os.mkdir(dir_path + "/outputs")
+
+    asyncio.run(main_async())
 
 
 if __name__ == "__main__":
